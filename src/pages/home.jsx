03431a84aaad42b7d7357347018594a7a26b513e
Abdullah12-tech <abdullahakinkunmi26@gmail.com>
2025-06-29 08:28:33 +0100

    solved

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,7 @@
 
 import Contact from "../components/contact"
 import Hero from "../components/HeroSection";
-import Skills from "../components/skills";
+import Skills from "../components/Skills";
 import Experience from "../components/Experience"
 import Testimonials from "../components/Testimonial";
 import WhatICanDo from "../components/WhatICando";
